f2e6b84f8671d888385ff3ea5d2f284cb0e8a47e
dnldsht <dnld.sht@gmail.com>
2023-05-28 08:33:21 +0200

    feat: more projects

--- src/components/projectsList/_projectList.ts
+++ src/components/projectsList/_projectList.ts
@@ -26,6 +26,20 @@ export const projects: Projects<Project> = [
     previewLink: 'https://qipo.it',
   },
   {
+    projectName: 'vue-insta-stories',
+    description: 'Instagram stories component for Vue 2/3',
+    previewLink: 'https://vue-insta-stories.donld.me',
+    repo: {
+      link: 'https://github.com/dnldsht/vue-insta-stories',
+      host: 'github',
+    },
+  },
+  {
+    projectName: 'master-thesis',
+    description: 'Incomplete time-series classification methods',
+    previewLink: 'https://github.com/dnldsht/master-thesis',
+  },
+  {
     projectName: '@we/dt',
     description: 'Powerful and flexible data table for Vue.js',
     previewLink: 'https://nuxt.weconstudio.it/datatable',
@@ -33,5 +47,19 @@ export const projects: Projects<Project> = [
       link: 'https://git.weconstudio.it/we/dt',
       host: 'gitlab',
     },
+  },
+  {
+    projectName: 'mobile-preview',
+    description: 'Preview any website on popular mobile devices',
+    previewLink: 'https://mobile.donld.me',
+    repo: {
+      link: 'https://github.com/dnldsht/mobile-preview',
+      host: 'github',
+    },
+  },
+  {
+    projectName: 'Leafpic',
+    description: 'Material designed Android gallery app',
+    previewLink: 'https://github.com/UnevenSoftware/LeafPic',
   },
 ]
